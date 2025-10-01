e396959468ee1516465bdf90503ba8486f8b34c5
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2025-10-01 11:44:59 +0200

    Added a few permissions using seeders

---
+++ src/server/db/Seeders/SeedPermissions.ts
@@ -0,0 +1,27 @@
+import { permissions } from "~/server/db/schema";
+import { db } from "~/server/db";
+
+interface Permission {
+  name: string;
+  resource: string;
+  action: "create" | "read" | "update" | "delete";
+  id?: number;
+  createdAt?: Date;
+  description?: string;
+}
+
+export async function seedPermissions() {
+  const predefinedPermissions: Permission[] = [
+    { id: 1, name: "create leave request", action: "create", resource: "leave request", createdAt: new Date() },
+    { id: 2, name: "read leave request", action: "read", resource: "leave request", createdAt: new Date() },
+    { id: 3, name: "update leave request", action: "update", resource: "leave request", createdAt: new Date() },
+    { id: 3, name: "delete leave request", action: "update", resource: "leave request", createdAt: new Date() },
+  ];
+
+  // eslint-disable-next-line drizzle/enforce-delete-with-where
+  await db.delete(permissions);
+
+  await db.insert(permissions).values(predefinedPermissions).onConflictDoNothing().execute();
+
+  console.log("Permissions seeded");
+}
--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,10 +1,12 @@
 import { seedRoles } from "~/server/db/Seeders/seedRoles";
 import { pool } from "~/server/db";
+import { seedPermissions } from "~/server/db/Seeders/SeedPermissions";
 
 async function main() {
   try {
     // Add your seeders here
     await seedRoles();
+    await seedPermissions();
 
     console.log("Seeding complete");
   } catch (err) {
