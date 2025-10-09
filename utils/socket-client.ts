691ddf4c0d66d7ea615e8703db84ed6bf11d3102
Maik Hendriks <123378700+MaikHDev@users.noreply.github.com>
2025-10-10 01:08:25 +0200

    Socket client update

--- utils/socket-client.ts
+++ utils/socket-client.ts
@@ -4,6 +4,11 @@ const socket = io(`${process.env.SOCKET_URL}:${process.env.SOCKET_PORT}`, {
     path: "/socket.io/",
     protocols: ["GET", "POST"],
     transports: ['websocket', 'polling'],
+    timeout: 20000,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+    autoConnect: true,
 });
 
 export const getSocket = () => {
