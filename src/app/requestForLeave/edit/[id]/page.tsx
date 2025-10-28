70fa5fd46f49af4bcd74f3b70bab160ce40ea80f
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-10-28 12:07:33 +0100

    Fixed build, made it client side for the params. Not static

--- src/app/requestForLeave/edit/[id]/page.tsx
+++ src/app/requestForLeave/edit/[id]/page.tsx
@@ -1,19 +1,11 @@
+"use client"
+
+import { useParams } from "next/navigation";
 import EditRequestForLeave from "../editRequestForLeave";
 
-export default function EditLeaveRequestPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default function EditLeaveRequestPage() {
+  const params = useParams();
   const requestId = Number(params.id);
 
-  if (isNaN(requestId)) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-red-600">Invalid request ID</p>
-      </div>
-    );
-  }
-
-  return <EditRequestForLeave requestId={requestId} />;
+  return <EditRequestForLeave requestId={requestId}/>;
 }
