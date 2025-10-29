1424a59d0afbf2682c651aacfac9cf8cc7fccebe
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-10-29 15:40:02 +0100

    remove unnassesary code and fix build

--- src/app/requestForLeave/edit/[id]/page.tsx
+++ src/app/requestForLeave/edit/[id]/page.tsx
@@ -2,13 +2,10 @@
 
 import { useParams } from "next/navigation";
 import EditRequestForLeave from "../editRequestForLeave";
-import { usePermission } from "~/hooks/usePermission";
 
 export default function EditLeaveRequestPage() {
   const params = useParams();
   const requestId = Number(params.id);
-
-  if(!usePermission('leaveRequest.edit')) return {message: "No access granted!"}
 
   return <EditRequestForLeave requestId={requestId}/>;
 }
