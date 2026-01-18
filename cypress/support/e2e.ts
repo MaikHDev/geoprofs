9a33c4376632e16639a965a41b1a93342abc09a6
Maik Hendriks <maikew2006@gmail.com>
2026-01-18 19:55:30 +0100

    setup

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,15 @@
+// Import commands.js using ES2015 syntax:
+import "./commands";
+
+// Hide fetch/XHR requests from command log (optional, cleaner logs)
+const app = window.top;
+if (
+  app &&
+  !app.document.head.querySelector("[data-hide-command-log-request]")
+) {
+  const style = app.document.createElement("style");
+  style.innerHTML =
+    ".command-name-request, .command-name-xhr { display: none }";
+  style.setAttribute("data-hide-command-log-request", "");
+  app.document.head.appendChild(style);
+}
--- src/server/db/Seeders/SeedRolesHasPermissions.ts
+++ src/server/db/Seeders/SeedRolesHasPermissions.ts
@@ -1,5 +1,5 @@
 import { db } from "~/server/db";
-import { rolePermissions } from "~/server/db/schema";
+import { permissions, rolePermissions } from "~/server/db/schema";
 
 interface RolesHasPermissions {
   roleId: number;
@@ -9,17 +9,18 @@ interface RolesHasPermissions {
 }
 
 export async function seedRolesHasPermissions() {
-  const predefinedRolesHasPermissions: RolesHasPermissions[] = [
-    { id: "1", roleId: 1, permissionId: 1, assignedAt: new Date() },
-    { id: "2", roleId: 1, permissionId: 2, assignedAt: new Date()  },
-    { id: "3", roleId: 1, permissionId: 3, assignedAt: new Date()  },
-    { id: "4", roleId: 1, permissionId: 4, assignedAt: new Date()  },
-  ];
+  const predefinedRolesHasPermissions: RolesHasPermissions[] = [];
+
+  const perms = db.select().from(permissions);
 
   // eslint-disable-next-line drizzle/enforce-delete-with-where
   await db.delete(rolePermissions);
 
-  await db.insert(rolePermissions).values(predefinedRolesHasPermissions).onConflictDoNothing().execute();
+  await db
+    .insert(rolePermissions)
+    .values(predefinedRolesHasPermissions)
+    .onConflictDoNothing()
+    .execute();
 
   console.log("RolesHasPermissions seeded");
 }
